a45ddf4bc8b5c8ff227a174b3948e18f3549a462
setutu <mail@setutu.com>
2025-05-21 15:49:44 -0700

    feat: api service returns 9 random images

--- apps/server/src/api-routes/v1/index.ts
+++ apps/server/src/api-routes/v1/index.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
+import { getRandomUniqueIntegers } from "src/libs/generate-random-integers";
 
 export const routes = async (
   fastify: FastifyInstance,
@@ -7,8 +8,34 @@ export const routes = async (
   fastify.addHook("preHandler", options.preHandler.apiGuard);
 
   fastify.get("/images", async (req, res) => {
-    res.send({
-      message: "Hello World",
-    });
+    try {
+      // fetch total image count
+      const pgConn = await fastify.pg.connect();
+      const result = await pgConn.query("SELECT COUNT(*) FROM image");
+      const totalImageCount = parseInt(result.rows[0].count, 10);
+
+      // random generate random 9 integers between 1 and total image count
+      const randomIds = getRandomUniqueIntegers(9, totalImageCount);
+      // !TODO: check redis cache first
+
+      const { rows: randomImages } = await pgConn.query(
+        `SELECT * FROM image WHERE id = ANY($1::int[])`,
+        [randomIds]
+      );
+
+      return {
+        code: 200,
+        data: {
+          images: randomImages,
+        },
+        message: "success",
+      };
+    } catch (error) {
+      return {
+        code: 500,
+        data: null,
+        message: `Internal server error: ${error}`,
+      };
+    }
   });
 };
