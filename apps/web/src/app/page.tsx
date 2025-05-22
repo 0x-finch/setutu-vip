daa2468ea298a5884672257518069a0f0482bf6d
setutu <mail@setutu.com>
2025-05-22 10:23:36 -0700

    fix: adjust margin for improved layout on HomePage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,7 @@ import { pgQueryImages } from "src/libs/pg-client";
 export default async function HomePage() {
   const randomImages = await pgQueryImages(20);
   return (
-    <div className="flex flex-col items-center gap-4 mx-auto max-w-7xl w-full p-4 bg-gray-100 min-h-screen">
+    <div className="flex flex-col items-center gap-4 mx-auto mb-20 p-4 max-w-7xl w-full bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold">涩兔兔</h1>
       <PhotoCarousel randomImages={randomImages} />
       <RotateButton />
