df33ecc51ad3ba82f3c0b1ed4732cac9c891bc6b
Yumin Gui <guiyumin@protonmail.com>
2025-05-23 12:33:26 -0700

    fix: remove NextImage

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        pathname: "/media/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
