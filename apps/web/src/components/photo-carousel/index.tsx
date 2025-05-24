c3682e251d9bcb23965903e78ad85376fec92587
Yumin Gui <guiyumin@protonmail.com>
2025-05-24 12:07:36 -0700

    feat: dynamically calculate grid rows

--- apps/web/src/components/photo-carousel/index.tsx
+++ apps/web/src/components/photo-carousel/index.tsx
@@ -5,9 +5,15 @@ import "react-photo-view/dist/react-photo-view.css";
 import { Image } from "src/types";
 
 export const PhotoCarousel = ({ randomImages }: { randomImages?: Image[] }) => {
+  const count = randomImages?.length ?? 0;
+  const rows = Math.ceil(count / 2);
+  const xlRows = Math.ceil(count / 4);
+
   return (
     <PhotoProvider>
-      <div className="grid grid-cols-2 xl:grid-cols-4 grid-rows-10 xl:grid-rows-5 gap-4 w-96 xl:w-192 h-fit">
+      <div
+        className={`grid grid-cols-2 grid-rows-${rows} xl:grid-cols-4 xl:grid-rows-${xlRows} gap-4 w-96 xl:w-192 h-fit`}
+      >
         {randomImages?.map((image: Image, index: number) => (
           <PhotoView key={index} src={image.url}>
             <img
