c546be5c746a9017bb120841b2b807c23d2619b7
Yumin Gui <guiyumin@protonmail.com>
2025-05-24 23:50:35 -0700

    feat: use Suspense for loading

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,16 @@
-import { PhotoCarousel } from "src/components/photo-carousel";
+import { Suspense } from "react";
+import { PhotoFetch } from "src/components/photo-fetch";
 import { RotateButton } from "src/components/rotate-button";
-import { pgQueryImages } from "src/libs/pg-client";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const randomImages = await pgQueryImages(20);
   return (
     <div className="flex flex-col items-center gap-4 mx-auto mb-20 p-4 max-w-7xl w-full bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold">涩兔兔</h1>
-      <PhotoCarousel randomImages={randomImages} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PhotoFetch />
+      </Suspense>
       <RotateButton />
     </div>
   );
