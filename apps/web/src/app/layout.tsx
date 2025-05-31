d2a5353d8fe9f430483ce53d63941c23eac66aa4
setutu <mail@setutu.com>
2025-05-31 15:37:01 -0700

    Revert "fix: font"
    This reverts commit 33ac0f5a9f9eb385b5fe43c640998bdaa8bf59fd.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 export const metadata: Metadata = {
   title: "涩兔兔",
   description: "涩兔兔 - 一个你忘不了的网站",
@@ -23,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} min-h-screen w-full flex flex-col antialiased`}
-      >
+      <body className="min-h-screen w-full flex flex-col antialiased">
         {children}
       </body>
     </html>
