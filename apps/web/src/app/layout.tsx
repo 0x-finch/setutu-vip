33ac0f5a9f9eb385b5fe43c640998bdaa8bf59fd
Yumin Gui <guiyumin@protonmail.com>
2025-05-31 15:30:01 -0700

    fix: font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 export const metadata: Metadata = {
   title: "涩兔兔",
   description: "涩兔兔 - 一个你忘不了的网站",
@@ -13,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen w-full flex flex-col antialiased">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen w-full flex flex-col antialiased`}
+      >
         {children}
       </body>
     </html>
