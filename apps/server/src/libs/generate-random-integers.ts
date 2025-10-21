46b9b3af577058dd80b69438e8c33cc83d1dd13c
setutu <mail@setutu.com>
2025-10-21 12:02:19 -0700

    refactor: remove server-related files and configurations

--- apps/server/src/libs/generate-random-integers.ts
+++
@@ -1,7 +0,0 @@
-export function getRandomUniqueIntegers(count: number, max: number): number[] {
-  const set = new Set<number>();
-  while (set.size < count && set.size < max) {
-    set.add(Math.floor(Math.random() * max) + 1);
-  }
-  return Array.from(set);
-}
--- apps/server/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import "fastify";
-
-declare module "fastify" {
-  interface FastifyRequest {
-    api: {
-      email: string;
-    } | null;
-  }
-}
